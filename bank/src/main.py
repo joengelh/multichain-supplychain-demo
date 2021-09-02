ed3a923e612aa66803723bb2e858db964ac0c5cc
joengelh <73387330+joengelh@users.noreply.github.com>
2021-09-02 21:49:56 +0200

    get foundations for banking api

--- bank/src/main.py
+++ bank/src/main.py
@@ -2,7 +2,7 @@ import mcrpc
 import os
 from dotenv import load_dotenv
 import re
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
 import ast
@@ -30,20 +30,30 @@ class helloWorld(Resource):
     def get(self):
         return {"data":"hi"}
 
+class balance(Resource):
+    def post(self, account):
+        request.get_json()
+        client.importaddress(account)
+        client.getaddressbalances(account)
+        return {"data":"SUCCESS"}
+
 class fund(Resource):
     def post(self, account, amount):
         client.issuemore(account,"EUR",amount)
+        return {"data":"SUCCESS"}
 
 class refund(Resource):
-    def post(self, account, amount):
+    def post(self, amount):
         pattern = re.compile("(?<=burnaddress': ')[0-9][A-Za-z0-9]+")
         burnAddress = pattern.findall(str(client.getinfo()))[0]
         try:
             client.sendasset(burnAddress,"EUR",amount)
+            return {"data":"SUCCESS"}
         except:
-            return "ERROR: not enough funds available"
+            return {"data":"ERROR: not enough funds available"}
 
 api.add_resource(helloWorld, '/')
+api.add_resource(balance, '/api/v1/balance')
 api.add_resource(fund, '/api/v1/fund')
 api.add_resource(refund, '/api/v1/refund')
 
--- bank/src/test.py
+++ bank/src/test.py
@@ -2,7 +2,7 @@ import mcrpc
 import os
 from dotenv import load_dotenv
 import re
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
 import ast
@@ -23,16 +23,31 @@ try:
 except:
     pass
 
-def fund(account, amount):
-    client.issuemore(account,"EUR",amount)
+app = Flask(__name__)
+api = Api(app)
+
+class helloWorld(Resource):
+    def get(self):
+        return {"data":"hi"}
+
+class balance(Resource):
+    def post(self):
+        data = request.get_json()
+        client.importaddress(data['account'])
+        return {"data":client.getaddressbalances(data['account'])}
 
 
-def balance(account):
-    client.importaddress(account)
-    print(client.getaddressbalances(account))
+api.add_resource(helloWorld, '/')
+api.add_resource(balance, '/api/v1/balance')
 
-def refund(amount):
-    pattern = re.compile("(?<=burnaddress': ')[0-9][A-Za-z0-9]+")
-    burnAddress = pattern.findall(str(client.getinfo()))[0]
-    client.sendasset(burnAddress,"EUR",amount)
-refund(1)
+if __name__ == '__main__':
+    app.run()
+
+
+#for server in tradableServers:
+    #issue asset class for every server, quantitiy zero, devisible by 1
+#    client.issue(client.listaddresses()[0]['address'], {"name":server,"open":True},0,1)
+#client.create("steam","iot001",False)
+#client.issue(client.listaddresses()[0]['address'],"dellserver",600,1)
+#client.sendasset(burnAddress,"hpeServer",300)
+#
