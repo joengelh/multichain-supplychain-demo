63e5cc4d5cc219f003bd598e06cf98de943ca330
joengelh <73387330+joengelh@users.noreply.github.com>
2021-09-02 15:36:02 +0200

    remove duplicate getting started header

---
+++ tools/testConnection.py
@@ -0,0 +1,17 @@
+import mcrpc
+import ast
+import os
+from dotenv import load_dotenv
+import re
+
+load_dotenv()
+nodes = ast.literal_eval(os.getenv('NODES'))
+
+for node in nodes:
+    client = c = mcrpc.RpcClient(
+        node['ip'], 
+        node['port'], 
+        node['user'],
+        node['password']
+    )
+    print(client.getinfo())
