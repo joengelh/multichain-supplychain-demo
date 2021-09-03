5a0e95299b9aaedce4be5765a94b0b27d1cf3871
joengelh <73387330+joengelh@users.noreply.github.com>
2021-09-03 23:52:10 +0200

    finish bank frontend

--- frontend-bank/website/views.py
+++ frontend-bank/website/views.py
@@ -25,6 +25,35 @@ def home():
         json={'account': '17PBfmGiEUt8diTd3fD8idpnFPXiiiX4FacfzL'}).json()
     return render_template("home.html", user=current_user, balance=balance["data"]["qty"])
 
+@views.route('/fund', methods=['GET', 'POST'])
+@login_required
+def fund():
+    if request.method == 'POST':
+        amount = request.form['amount']
+        print(amount)
+        print(type(amount))
+        requests.post('http://localhost:5001/api/v1/fund', 
+            json={'account': current_user.wallet, 'amount': float(amount)})
+        flash('Funding Successful!', category='success')
+    #get current balance by calling the blockchain
+    balance = requests.post('http://localhost:5001/api/v1/balance', 
+        json={'account': current_user.wallet}).json()
+    return render_template("fund.html", user=current_user, balance=balance["data"]["qty"])
+
+@views.route('/refund', methods=['GET', 'POST'])
+@login_required
+def refund():
+    if request.method == 'POST':
+        amount = request.form['amount']
+        print(amount)
+        print(type(amount))
+        requests.post('http://localhost:5001/api/v1/refund', 
+            json={'amount': float(amount)})
+        flash('Refunding Successful!', category='success')
+    #get current balance by calling the blockchain
+    balance = requests.post('http://localhost:5001/api/v1/balance', 
+        json={'account': current_user.wallet}).json()
+    return render_template("refund.html", user=current_user, balance=balance["data"]["qty"])
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():
