7cc97633bc4006079aaf5e76a4b81f68dcb30015
joengelh <73387330+joengelh@users.noreply.github.com>
2021-09-05 17:47:30 +0200

    finish iot api

--- README.md
+++ README.md
@@ -26,11 +26,11 @@ demo of a supplychain on multichain using the blockchain deployment from [Github
 2. customer communicates serviceProvider the business needs :thumbsup:
 3. serviceProvider communicates oem the besiness needs :thumbsup:
 4. oem issues asset & stream
-5. logistics writes to stream
+5. iot writes to stream
 6. oem offers atomic transaction server against EUR
 7. service provider reviews data, accepts
 8. service provider writes to steam
-9. logistics writes to stream
+9. iot writes to stream
 10. service provider offers atomic transaction, server against EUR
 11. customer reviews data, accepts
 
---
+++ api-iot/main.py
@@ -0,0 +1,58 @@
+import mcrpc
+import os
+from dotenv import load_dotenv
+import re
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+import ast
+
+#connect to banks multichain node
+load_dotenv()
+nodes = ast.literal_eval(os.getenv('NODES'))
+bank = next((node for node in nodes if node['name'] == 'iot'), None)
+client = c = mcrpc.RpcClient(
+        bank['ip'], 
+        bank['port'], 
+        bank['user'],
+        bank['password']
+    )
+
+#initiate falsk app
+app = Flask(__name__)
+api = Api(app)
+
+#create root path
+class helloWorld(Resource):
+    def get(self):
+        return {'data':'hi'}
+
+#create path to create stream, requires name
+class createStream(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            client.create("stream",data["name"],False)
+            return {'data':"Stream created!"}
+        except:
+            return {'data':"Stream already exists!"}
+
+#create path to publish to stream, requires name, key, data-json
+#postman example: {"name":"package001","key":"sensor","data-json":
+#{"json":{"temperatur":40,"humidity":400,"maxG":0.1,
+#"location":"N12314E123123"}}}
+class publish(Resource):
+    def post(self):
+        data = request.get_json()
+        client.publish(data["name"],data["key"],data["data-json"])
+        return {'data':data["data-json"]}
+
+api.add_resource(helloWorld, '/')
+api.add_resource(createStream, '/api/v1/createStream')
+api.add_resource(publish, '/api/v1/publish')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5005)
+
+
+
+
