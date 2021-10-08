2e8bff7acb91fcb8a892c8bb014694a1a75c85bd
joengelh <73387330+joengelh@users.noreply.github.com>
2021-10-08 15:38:45 +0200

    add run script

--- frontend-stakeHolder/main.py
+++ frontend-stakeHolder/main.py
@@ -1,11 +1,6 @@
 from website import create_app
 import sys
 
-#get port and name from arguments
-print("Give name of client as first, port as second argument like python3 frontend-shareHolder/main.py oem 5004")
-NAME = sys.argv[1]
-PORT = sys.argv[2]
-
 app = create_app()
 
 if __name__ == '__main__':
